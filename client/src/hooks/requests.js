97032525dbbdc5cdb1aa818b295605d04f476fad
MarcusFC3 <22795@my4county.net>
2024-12-18 14:42:08 -0500

    asdasdsadddd

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,5 +1,5 @@
 // URL of the API
-const API_URL = "https://healthy-habit-tracker-web-app.vercel.app";
+const API_URL = process.env.VERCEL_URL;
 
 // Function that sends a POST request to create an account
 async function httpRegisterAccount(accountData){
