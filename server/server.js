ac25fe0ca9730a481d5ea8585b8253cf01922072
Marcus Jones Jr. <22795@my4county.net>
2024-12-19 23:44:31 -0500

    fix no boy

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-"use strict"
+// "use strict"
 
-const express = require("express");
-const https = require("https");
-const app = require("./api/index");
-const sql = require("mssql");
-const fs = require("fs");
-const path = require("path");
+// const express = require("express");
+// const https = require("https");
+// const app = require("./api/index");
+// const sql = require("mssql");
+// const fs = require("fs");
+// const path = require("path");
 
-const PORT = process.env.PORT || 3000;;
-export default app;
-// const server = https.createServer({
-//     key: fs.readFileSync(path.join(__dirname, ".." , "key.pem")),
-//     cert: fs.readFileSync(path.join(__dirname,  ".." , "cert.pem")),
-// }, app); 
+// const PORT = process.env.PORT || 3000;;
+// export default app;
+// // const server = https.createServer({
+// //     key: fs.readFileSync(path.join(__dirname, ".." , "key.pem")),
+// //     cert: fs.readFileSync(path.join(__dirname,  ".." , "cert.pem")),
+// // }, app); 
 
-// server.listen(PORT, () => {
-//     console.log("Server is listening on port " + PORT);
-// })  
+// // server.listen(PORT, () => {
+// //     console.log("Server is listening on port " + PORT);
+// // })  
