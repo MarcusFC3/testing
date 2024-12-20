3e4c6789aa95e875c604be78fc3ab975f8de90f5
Marcus Jones Jr. <22795@my4county.net>
2024-12-19 23:52:28 -0500

    fix no boy

--- server/api/index.js
+++ server/api/index.js
@@ -171,9 +171,9 @@ app.get("/asd", (req, res)=>{
 })
 
 app.use("/activities", checkLoggedIn, activitiesRouter)
-app.use("/api/login", loginRouter);
+app.use("/login", loginRouter);
 app.use("/user", userRouter);
-app.get("/", (req, res) => {return res.send(req.user)})    
+app.get("/api", (req, res) => {return res.send("hyecuh")})    
 
 app.get("/logout", (req, res) =>{
   req.logout(()=>{
